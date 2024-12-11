15fbbf38737864f28e69ae655089698155c7864a
何冠峰 <hevinci@hotmail.com>
2024-12-11 18:24:40 +0800

    Update InitializeParameters.cs

--- Assets/YooAsset/Runtime/InitializeParameters.cs
+++ Assets/YooAsset/Runtime/InitializeParameters.cs
@@ -165,13 +165,27 @@ namespace YooAsset
         }
 
         /// <summary>
-        /// 创建默认的Web文件系统参数
+        /// 创建默认的WebServer文件系统参数
         /// </summary>
         /// <param name="disableUnityWebCache">禁用Unity的网络缓存</param>
-        public static FileSystemParameters CreateDefaultWebFileSystemParameters(bool disableUnityWebCache = false)
+        public static FileSystemParameters CreateDefaultWebServerFileSystemParameters(bool disableUnityWebCache = false)
         {
             string fileSystemClass = typeof(DefaultWebServerFileSystem).FullName;
             var fileSystemParams = new FileSystemParameters(fileSystemClass, null);
+            fileSystemParams.AddParameter(FileSystemParametersDefine.DISABLE_UNITY_WEB_CACHE, disableUnityWebCache);
+            return fileSystemParams;
+        }
+
+        /// <summary>
+        /// 创建默认的WebRemote文件系统参数
+        /// </summary>
+        /// <param name="remoteServices">远端资源地址查询服务类</param>
+        /// <param name="disableUnityWebCache">禁用Unity的网络缓存</param>
+        public static FileSystemParameters CreateDefaultWebRemoteFileSystemParameters(IRemoteServices remoteServices, bool disableUnityWebCache = false)
+        {
+            string fileSystemClass = typeof(DefaultWebServerFileSystem).FullName;
+            var fileSystemParams = new FileSystemParameters(fileSystemClass, null);
+            fileSystemParams.AddParameter(FileSystemParametersDefine.REMOTE_SERVICES, remoteServices);
             fileSystemParams.AddParameter(FileSystemParametersDefine.DISABLE_UNITY_WEB_CACHE, disableUnityWebCache);
             return fileSystemParams;
         }
